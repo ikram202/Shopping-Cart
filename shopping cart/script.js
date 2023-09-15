08388b3129759661870e9305ea4f4696de7c4b69
LOUARNI IKRAM <116180576+ikram202@users.noreply.github.com>
2023-09-15 09:54:16 +0100

    Update script.js

--- shopping cart/script.js
+++ shopping cart/script.js
@@ -2,39 +2,44 @@ const quantityInputs = document.querySelectorAll('.product-quantity');
 const priceElements = document.querySelectorAll('.product-price');
 const linePriceElements = document.querySelectorAll('.product-line-price');
 const cartSubtotal = document.getElementById('cart-subtotal');
-const heartElements = document.querySelectorAll('.heart');
-const trashIcons = document.querySelectorAll('.trash');
+
+console.log(quantityInputs)
 
 function updateTotal() {
   let totalPrice = 0;
-
-  for (let i = 0; i < quantityInputs.length; i++) {
+  for (let i = 1; i < quantityInputs.length; i++) { 
     const quantity = parseInt(quantityInputs[i].value);
-    const price = parseFloat(priceElements[i].textContent.replace('$', ''));
+    const price = parseFloat(priceElements[i].textContent.replace('$', '')); 
     const linePrice = quantity * price;
-    
-    linePriceElements[i].textContent = `$${linePrice.toFixed(2)}`;
+    linePriceElements[i].textContent = `${linePrice}€`;
     totalPrice += linePrice;
   }
-  
-  cartSubtotal.textContent = `$${totalPrice.toFixed(2)}`;
+  cartSubtotal.textContent = `${totalPrice}€`; 
 }
 
-quantityInputs.forEach(input => {
+quantityInputs.forEach(function (input) {
   input.addEventListener('input', updateTotal);
 });
 
-heartElements.forEach(element => {
-  element.addEventListener('click', event => {
-    event.target.classList.toggle('selected');
-  });
+function toggleHeartColor(e) {
+  const clickedHeart = e.target;
+  clickedHeart.classList.toggle('selected');
+}
+
+const heartElements = document.querySelectorAll('.heart');
+heartElements.forEach((element) => {
+  element.addEventListener('click', toggleHeartColor);
 });
 
-trashIcons.forEach(trashIcon => {
+updateTotal();
+
+document.querySelectorAll('.trash').forEach((trashIcon, index) => {
   trashIcon.addEventListener('click', () => {
-    const row = trashIcon.closest('tr');
+    const row = trashIcon.parentElement.parentElement;
     row.remove();
+
     updateTotal();
+    
   });
 });
 
